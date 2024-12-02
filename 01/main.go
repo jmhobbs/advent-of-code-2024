12a3ee806cc4e1a9eb96a0ecbb3d461d01e03553
John Hobbs <john@velvetcache.org>
2024-12-01 20:32:32 -0600

    Day 01 - Problem A - Solution

--- 01/main.go
+++ 01/main.go
@@ -1,9 +1,66 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	var (
+		left  []int
+		right []int
+		split []string
+		value int64
+	)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		split = strings.SplitN(scanner.Text(), "   ", 2)
+		value, err = strconv.ParseInt(split[0], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		left = append(left, int(value))
+
+		value, err = strconv.ParseInt(split[1], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		right = append(right, int(value))
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("A: %d\n", ListDistance(left, right))
 }
 
-func ListDistances(left []int, right []int) int {
-	return 0
+func ListDistance(left []int, right []int) int {
+	slices.Sort(left)
+	slices.Sort(right)
+
+	var distance int
+
+	for i, l := range left {
+		distance += abs(l - right[i])
+	}
+
+	return distance
+}
+
+func abs(val int) int {
+	if val < 0 {
+		return -val
+	}
+	return val
 }
--- 01/main_test.go
+++ 01/main_test.go
@@ -6,9 +6,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_ListDistances(t *testing.T) {
+func Test_ListDistance(t *testing.T) {
 	left := []int{3, 4, 2, 1, 3, 3}
 	right := []int{4, 3, 5, 3, 9, 3}
 
-	assert.Equal(t, 11, ListDistances(left, right))
+	assert.Equal(t, 11, ListDistance(left, right))
 }
