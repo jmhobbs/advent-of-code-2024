52b75d676421d6746c23e88933894af715a48b3e
John Hobbs <john@velvetcache.org>
2024-12-03 11:42:52 -0600

    Day 03 - Problem B - Solution

--- 03/main.go
+++ 03/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"regexp"
@@ -13,41 +14,77 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("A: %d\n", SumMultipliersFromCorruptedMemory(buf))
+	ops := ExtractOperations(buf)
+
+	fmt.Printf("A: %d\n", SumMultipliers(ops))
+	fmt.Printf("B: %d\n", SumMultipliersWithEnablers(ops))
 }
 
-type Mul struct {
+type OpName uint8
+
+const (
+	Do OpName = iota
+	Dont
+	Mul
+)
+
+type Op struct {
+	Name  OpName
 	Left  int
 	Right int
 }
 
-var regexpMul = regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+var regexpMul = regexp.MustCompile(`(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))`)
 
-func ExtractMultipliers(input []byte) []Mul {
-	muls := []Mul{}
+func ExtractOperations(input []byte) []Op {
+	ops := []Op{}
 	matches := regexpMul.FindAllSubmatch(input, -1)
 	for _, match := range matches {
-		left, err := strconv.ParseInt(string(match[1]), 10, 64)
+		if bytes.Equal(match[1], []byte("do()")) {
+			ops = append(ops, Op{Do, 0, 0})
+			continue
+		}
+		if bytes.Equal(match[1], []byte("don't()")) {
+			ops = append(ops, Op{Dont, 0, 0})
+			continue
+		}
+		left, err := strconv.ParseInt(string(match[2]), 10, 64)
 		if err != nil {
 			panic(err)
 		}
-		right, err := strconv.ParseInt(string(match[2]), 10, 64)
+		right, err := strconv.ParseInt(string(match[3]), 10, 64)
 		if err != nil {
 			panic(err)
 		}
-		muls = append(muls, Mul{int(left), int(right)})
+		ops = append(ops, Op{Mul, int(left), int(right)})
 	}
-	return muls
+	return ops
 }
 
-func SumMultipliersFromCorruptedMemory(input []byte) int {
+func SumMultipliers(input []Op) int {
 	sum := 0
-	for _, m := range ExtractMultipliers(input) {
-		sum += m.Left * m.Right
+	for _, m := range input {
+		if m.Name == Mul {
+			sum += m.Left * m.Right
+		}
 	}
 	return sum
 }
 
-func SumMultipliersFromCorruptedMemoryWithEnablers(input []byte) int {
-	return 0
+func SumMultipliersWithEnablers(input []Op) int {
+	sum := 0
+	enabled := true
+	for _, m := range input {
+		switch m.Name {
+		case Do:
+			enabled = true
+		case Dont:
+			enabled = false
+		case Mul:
+			if enabled {
+				sum += m.Left * m.Right
+			}
+		}
+	}
+	return sum
 }
--- 03/main_test.go
+++ 03/main_test.go
@@ -8,56 +8,95 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_ExtractMultipliers(t *testing.T) {
+func Test_ExtractOperations(t *testing.T) {
 	tests := []struct {
 		input    string
-		expected []main.Mul
+		expected []main.Op
 	}{
 		// `mul(44,46)` multiplies `44` by `46` to get a result of `2024`.
 		{
 			"mul(44,46)",
-			[]main.Mul{{44, 46}},
+			[]main.Op{{main.Mul, 44, 46}},
 		},
 		// Similarly, `mul(123,4)` would multiply `123` by `4`.
 		{
 			"mul(123,4)",
-			[]main.Mul{{123, 4}},
+			[]main.Op{{main.Mul, 123, 4}},
 		},
 		// `mul(4*`, `mul(6,9!`, `?(12,34)`, or `mul ( 2 , 4 )` do nothing.
 		{
 			"mul(4*",
-			[]main.Mul{},
+			[]main.Op{},
 		},
 		{
 			"mul(6,9!",
-			[]main.Mul{},
+			[]main.Op{},
 		},
 		{
 			"?(12,34)",
-			[]main.Mul{},
+			[]main.Op{},
 		},
 		{
 			"mul ( 2 , 4 )",
-			[]main.Mul{},
+			[]main.Op{},
+		},
+		{
+			// xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+			"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
+			// ...produces 161 (2*4 + 5*5 + 11*8 + 8*5)
+			[]main.Op{
+				{main.Mul, 2, 4},
+				{main.Mul, 5, 5},
+				{main.Mul, 11, 8},
+				{main.Mul, 8, 5},
+			},
+		},
+		{
+			// For example:
+			//   xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+			"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+			// This time, the sum of the results is 48 (2*4 + 8*5).
+			[]main.Op{
+				{main.Mul, 2, 4},
+				{main.Dont, 0, 0},
+				{main.Mul, 5, 5},
+				{main.Mul, 11, 8},
+				{main.Do, 0, 0},
+				{main.Mul, 8, 5},
+			},
 		},
 	}
 
 	for _, test := range tests {
-		assert.Equal(t, test.expected, main.ExtractMultipliers([]byte(test.input)))
+		assert.Equal(t, test.expected, main.ExtractOperations([]byte(test.input)))
 	}
 }
 
-func Test_SumMultipliersFromCorruptedMemory(t *testing.T) {
+func Test_SumMultipliers(t *testing.T) {
 	// xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
-	input := []byte("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+	input :=
+		[]main.Op{
+			{main.Mul, 2, 4},
+			{main.Mul, 5, 5},
+			{main.Mul, 11, 8},
+			{main.Mul, 8, 5},
+		}
 	// ...produces 161 (2*4 + 5*5 + 11*8 + 8*5)
-	assert.Equal(t, 161, main.SumMultipliersFromCorruptedMemory(input))
+	assert.Equal(t, 161, main.SumMultipliers(input))
 }
 
-func Test_SumMultipliersFromCorruptedMemoryWithEnablers(t *testing.T) {
+func Test_SumMultipliersWithEnablers(t *testing.T) {
 	// For example:
 	//   xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
-	input := []byte("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+	input := []main.Op{
+		{main.Mul, 2, 4},
+		{main.Dont, 0, 0},
+		{main.Mul, 5, 5},
+		{main.Mul, 32, 64},
+		{main.Mul, 11, 8},
+		{main.Do, 0, 0},
+		{main.Mul, 8, 5},
+	}
 	// This time, the sum of the results is 48 (2*4 + 8*5).
-	assert.Equal(t, 48, main.SumMultipliersFromCorruptedMemoryWithEnablers(input))
+	assert.Equal(t, 48, main.SumMultipliersWithEnablers(input))
 }
