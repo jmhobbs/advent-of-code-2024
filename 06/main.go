0155061fd2fad632f63d3be287eb920bedcc676a
John Hobbs <john@velvetcache.org>
2024-12-06 20:00:56 -0600

    Day 06 - Problem A - Solution

--- 06/main.go
+++ 06/main.go
@@ -2,47 +2,137 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
-	"os"
+
+	"github.com/jmhobbs/advent-of-code-2024/util"
 )
 
 func main() {
-	f, err := os.Open("input.txt")
-	if err != nil {
-		panic(err)
-	}
+	f := util.OpenInput()
 	defer f.Close()
 
-	_, _, err = ParseInput(f)
+	mmap, robot, err := ParseInput(f)
 	if err != nil {
 		panic(err)
 	}
+
+	WalkMap(&robot, mmap)
+
+	fmt.Printf("A: %d\n", CountVisitedPositions(mmap))
 }
 
 type Robot struct {
 	X, Y        int
-	Orientation byte
+	Orientation rune
 }
 
 type Map [][]byte
 
+func (m Map) String() string {
+	var s string
+	for _, line := range m {
+		s += string(line) + "\n"
+	}
+	return s
+}
+
 func ParseInput(in io.Reader) (Map, Robot, error) {
-	lines := [][]byte{}
+	var (
+		robot Robot
+		line  []byte
+		lines [][]byte
+	)
 
 	scanner := bufio.NewScanner(in)
 	for scanner.Scan() {
-		lines = append(lines, scanner.Bytes())
+		line = make([]byte, len(scanner.Bytes()))
+		copy(line, scanner.Bytes())
+
+		for x, c := range line {
+			if c == '^' || c == 'v' || c == '<' || c == '>' {
+				robot = Robot{x, len(lines), rune(c)}
+				line[x] = 'X'
+			}
+		}
+		lines = append(lines, line)
 	}
 
-	return Map(lines), Robot{0, 0, '?'}, scanner.Err()
+	return Map(lines), robot, scanner.Err()
 }
 
-func Step(robot Robot, mmap Map) bool {
-	return false
+func Step(robot *Robot, mmap Map) bool {
+	for i := 0; i < 4; i++ {
+		switch robot.Orientation {
+		case '^':
+			if robot.Y == 0 {
+				robot.Y--
+				return true
+			}
+			if mmap[robot.Y-1][robot.X] == '#' {
+				robot.Orientation = '>'
+				continue
+			}
+			robot.Y--
+			mmap[robot.Y][robot.X] = 'X'
+			return false
+		case '>':
+			if robot.X == len(mmap[robot.Y])-1 {
+				robot.X++
+				return true
+			}
+			if mmap[robot.Y][robot.X+1] == '#' {
+				robot.Orientation = 'v'
+				continue
+			}
+			robot.X++
+			mmap[robot.Y][robot.X] = 'X'
+			return false
+		case 'v':
+			if robot.Y == len(mmap)-1 {
+				robot.Y++
+				return true
+			}
+			if mmap[robot.Y+1][robot.X] == '#' {
+				robot.Orientation = '<'
+				continue
+			}
+			robot.Y++
+			mmap[robot.Y][robot.X] = 'X'
+			return false
+		case '<':
+			if robot.X == 0 {
+				robot.X--
+				return true
+			}
+			if mmap[robot.Y][robot.X-1] == '#' {
+				robot.Orientation = '^'
+				continue
+			}
+			robot.X--
+			mmap[robot.Y][robot.X] = 'X'
+			return false
+		}
+	}
+	panic("robot deadlocked")
 }
 
-func WalkMap(robot Robot, mmap Map) {}
+func WalkMap(robot *Robot, mmap Map) {
+	for {
+		if Step(robot, mmap) {
+			break
+		}
+	}
+}
 
 func CountVisitedPositions(mmap Map) int {
-	return 0
+	ctr := 0
+	for _, line := range mmap {
+		for _, c := range line {
+			if c == 'X' {
+				ctr++
+			}
+		}
+	}
+	return ctr
 }
--- 06/main_test.go
+++ 06/main_test.go
@@ -39,7 +39,191 @@ func Test_ParseInput(t *testing.T) {
 		},
 		mmap,
 	)
-	assert.Equal(t, main.Robot{4, 6, '?'}, robot)
+	assert.Equal(t, main.Robot{4, 6, '^'}, robot)
+}
+
+func Test_Step_Minimal(t *testing.T) {
+	t.Run("off the top", func(t *testing.T) {
+		mmap := main.Map{
+			[]byte("..."),
+			[]byte("..."),
+			[]byte("..."),
+		}
+		robot := main.Robot{1, 0, '^'}
+		assert.True(t, main.Step(&robot, mmap))
+		assert.Equal(t, main.Robot{1, -1, '^'}, robot)
+	})
+
+	t.Run("off the bottom", func(t *testing.T) {
+		mmap := main.Map{
+			[]byte("..."),
+			[]byte("..."),
+			[]byte("..."),
+		}
+		robot := main.Robot{1, 2, 'v'}
+		assert.True(t, main.Step(&robot, mmap))
+		assert.Equal(t, main.Robot{1, 3, 'v'}, robot)
+	})
+
+	t.Run("off the left", func(t *testing.T) {
+		mmap := main.Map{
+			[]byte("..."),
+			[]byte("..."),
+			[]byte("..."),
+		}
+		robot := main.Robot{0, 1, '<'}
+		assert.True(t, main.Step(&robot, mmap))
+		assert.Equal(t, main.Robot{-1, 1, '<'}, robot)
+	})
+
+	t.Run("off the right", func(t *testing.T) {
+		mmap := main.Map{
+			[]byte("..."),
+			[]byte("..."),
+			[]byte("..."),
+		}
+		robot := main.Robot{2, 1, '>'}
+		assert.True(t, main.Step(&robot, mmap))
+		assert.Equal(t, main.Robot{3, 1, '>'}, robot)
+	})
+
+	t.Run("up - blocked", func(t *testing.T) {
+		mmap := main.Map{
+			[]byte(".#."),
+			[]byte("..."),
+			[]byte("..."),
+		}
+		robot := main.Robot{1, 1, '^'}
+		assert.False(t, main.Step(&robot, mmap))
+		assert.Equal(t, main.Robot{2, 1, '>'}, robot)
+
+		assert.Equal(
+			t,
+			main.Map{
+				[]byte(".#."),
+				[]byte("..X"),
+				[]byte("..."),
+			},
+			mmap,
+		)
+	})
+
+	t.Run("right - blocked", func(t *testing.T) {
+		mmap := main.Map{
+			[]byte("..."),
+			[]byte("..#"),
+			[]byte("..."),
+		}
+		robot := main.Robot{1, 1, '>'}
+		assert.False(t, main.Step(&robot, mmap))
+		assert.Equal(t, main.Robot{1, 2, 'v'}, robot)
+
+		assert.Equal(
+			t,
+			main.Map{
+				[]byte("..."),
+				[]byte("..#"),
+				[]byte(".X."),
+			},
+			mmap,
+		)
+	})
+
+	t.Run("down - blocked", func(t *testing.T) {
+		mmap := main.Map{
+			[]byte("..."),
+			[]byte("..."),
+			[]byte(".#."),
+		}
+		robot := main.Robot{1, 1, 'v'}
+		assert.False(t, main.Step(&robot, mmap))
+		assert.Equal(t, main.Robot{0, 1, '<'}, robot)
+
+		assert.Equal(
+			t,
+			main.Map{
+				[]byte("..."),
+				[]byte("X.."),
+				[]byte(".#."),
+			},
+			mmap,
+		)
+	})
+
+	t.Run("left - blocked", func(t *testing.T) {
+		mmap := main.Map{
+			[]byte("..."),
+			[]byte("#.."),
+			[]byte("..."),
+		}
+		robot := main.Robot{1, 1, '<'}
+		assert.False(t, main.Step(&robot, mmap))
+		assert.Equal(t, main.Robot{1, 0, '^'}, robot)
+
+		assert.Equal(
+			t,
+			main.Map{
+				[]byte(".X."),
+				[]byte("#.."),
+				[]byte("..."),
+			},
+			mmap,
+		)
+	})
+
+	t.Run("up - clear", func(t *testing.T) {
+		mmap := main.Map{
+			[]byte("..."),
+			[]byte("..."),
+			[]byte("..."),
+		}
+		robot := main.Robot{1, 1, '^'}
+		assert.False(t, main.Step(&robot, mmap))
+		assert.Equal(t, main.Robot{1, 0, '^'}, robot)
+
+		assert.Equal(
+			t,
+			main.Map{
+				[]byte(".X."),
+				[]byte("..."),
+				[]byte("..."),
+			},
+			mmap,
+		)
+	})
+
+	t.Run("right - clear", func(t *testing.T) {
+		mmap := main.Map{
+			[]byte("..."),
+			[]byte("..X"),
+			[]byte("..."),
+		}
+		robot := main.Robot{1, 1, '>'}
+		assert.False(t, main.Step(&robot, mmap))
+		assert.Equal(t, main.Robot{2, 1, '>'}, robot)
+	})
+
+	t.Run("down - clear", func(t *testing.T) {
+		mmap := main.Map{
+			[]byte("..."),
+			[]byte("..."),
+			[]byte(".X."),
+		}
+		robot := main.Robot{1, 1, 'v'}
+		assert.False(t, main.Step(&robot, mmap))
+		assert.Equal(t, main.Robot{1, 2, 'v'}, robot)
+	})
+
+	t.Run("left - clear", func(t *testing.T) {
+		mmap := main.Map{
+			[]byte("..."),
+			[]byte("X.."),
+			[]byte("..."),
+		}
+		robot := main.Robot{1, 1, '<'}
+		assert.False(t, main.Step(&robot, mmap))
+		assert.Equal(t, main.Robot{0, 1, '<'}, robot)
+	})
 }
 
 func Test_Step(t *testing.T) {
@@ -60,7 +244,7 @@ func Test_Step(t *testing.T) {
 
 	// Following the above protocol, the guard moves up several times until she reaches an obstacle
 	for i := 0; i < 5; i++ {
-		assert.False(t, main.Step(robot, mmap))
+		assert.False(t, main.Step(&robot, mmap))
 	}
 	assert.Equal(t, main.Robot{4, 1, '^'}, robot)
 	assert.Equal(t, main.Map{
@@ -77,7 +261,7 @@ func Test_Step(t *testing.T) {
 	}, mmap)
 
 	// Because there is now an obstacle in front of the guard, she turns right before continuing straight in her new facing direction:
-	assert.False(t, main.Step(robot, mmap))
+	assert.False(t, main.Step(&robot, mmap))
 	assert.Equal(t, main.Robot{5, 1, '>'}, robot)
 	assert.Equal(t, main.Map{
 		[]byte("....#....."),
@@ -93,7 +277,7 @@ func Test_Step(t *testing.T) {
 	}, mmap)
 
 	for i := 0; i < 3; i++ {
-		assert.False(t, main.Step(robot, mmap))
+		assert.False(t, main.Step(&robot, mmap))
 	}
 	assert.Equal(t, main.Robot{8, 1, '>'}, robot)
 	assert.Equal(t, main.Map{
@@ -110,7 +294,7 @@ func Test_Step(t *testing.T) {
 	}, mmap)
 
 	// Reaching another obstacle (a spool of several very long polymers), she turns right again and continues downward
-	assert.False(t, main.Step(robot, mmap))
+	assert.False(t, main.Step(&robot, mmap))
 	assert.Equal(t, main.Robot{8, 2, 'v'}, robot)
 	assert.Equal(t, main.Map{
 		[]byte("....#....."),
@@ -142,7 +326,7 @@ func Test_WalkMap(t *testing.T) {
 
 	robot := main.Robot{4, 6, '^'}
 
-	main.WalkMap(robot, mmap)
+	main.WalkMap(&robot, mmap)
 
 	assert.Equal(t, main.Robot{7, 10, 'v'}, robot)
 	assert.Equal(t, main.Map{
